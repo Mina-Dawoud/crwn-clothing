085078b2d9253d3ce690d85630c16f1344a602c4
DawoudMina <mina.dawood1@vodafone.com>
2021-01-10 19:46:37 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import ShopPage from './pages/collection-page/shop.component';
 import HomePage from './pages/home-page/home-page.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
