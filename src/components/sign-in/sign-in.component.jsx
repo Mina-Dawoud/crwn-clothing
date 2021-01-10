1ac02dc0fd1df114da7494cc4c21c18b26fb9f13
DawoudMina <mina.dawood1@vodafone.com>
2021-01-10 20:12:45 +0200

    adding custom button component and complete sign in UI

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -29,7 +30,7 @@ class SignIn extends React.Component {
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+                <span className='title'>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
                     <FormInput
                         name='email'
@@ -48,7 +49,7 @@ class SignIn extends React.Component {
                         label='password'
                     />
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit' >Sign in</CustomButton>
                 </form>
             </div>
         )
